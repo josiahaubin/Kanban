7842dac31c5119adff4a20cf70d1e536a17014f0
Josiah Aubin <josiahaubin@gmail.com>
2019-09-16 11:44:46 -0600

    routes connected

--- server/main.js
+++ server/main.js
@@ -38,9 +38,13 @@ server.use('/account', new AuthController().router)
 //YOUR ROUTES HERE!!!!!!
 import BoardController from './controllers/BoardController'
 import ListController from './controllers/ListController'
+import TaskController from './controllers/TaskController'
+import CommentController from './controllers/CommentController'
 
 server.use('/api/boards', new BoardController().router)
 server.use('/api/lists', new ListController().router)
+server.use('/api/tasks', new TaskController().router)
+server.use('/api/comments', new CommentController().router)
 
 
 //Default Error Handler
