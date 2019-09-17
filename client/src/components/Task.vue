482e0236d897c416d4bf788c5e9c985ce04e8ca4
Josiah Aubin <josiahaubin@gmail.com>
2019-09-17 13:41:22 -0600

    Comments showing up

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="task border rounded">
     <p>{{taskProp.description}}</p>
+    <ul>
+      <comment v-for="comment in comments" :commentProp="comment" :key="comment._id" />
+    </ul>
   </div>
 </template>
 
 
 <script>
+import comment from "../components/Comment";
 export default {
   name: "task",
   props: ["taskProp"],
   data() {
     return {};
   },
-  computed: {},
-  methods: {},
-  components: {}
+  mounted() {
+    this.getComments();
+  },
+  computed: {
+    comments() {
+      return this.$store.state.comments[this.taskProp._id] || [];
+    }
+  },
+  methods: {
+    getComments() {
+      this.$store.dispatch("getComments", this.taskProp._id);
+    }
+  },
+  components: { comment }
 };
 </script>
 
